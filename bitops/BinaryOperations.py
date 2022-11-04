dc025c3df6931cedd587c0b179aa2e7182d7a5d7
Vitalij Gotovskij <vitalij@cdeveloper.eu>
2022-11-05 00:42:05 +0200

    Covered with unit-tests

--- bitops/BinaryOperations.py
+++ bitops/BinaryOperations.py
@@ -1,13 +1,23 @@
-import aenum
+from enum import Enum, auto
 
 
-class ENDIANNESS(aenum.AutoNumberEnum):
-    UNKLNOWN = (),
-    BIG      = (),
-    LITTLE   = (),
+class ENDIANNESS(Enum):
+    UNKLNOWN = 0,
+    BIG      = auto(),
+    LITTLE   = auto(),
 
 
-def get_bit(value, n, endiannes = ENDIANNESS.BIG):
+def get_effective_length_in_bytes(value):
+    bit_length = value.bit_length()
+    length_bytes = int(bit_length/8)
+    if(bit_length%8 > 0):
+        length_bytes += 1
+    if (length:=int(length_bytes)) == 0:
+        return 1
+    else:
+        return length
+
+def is_bit_set(value, n, endiannes = ENDIANNESS.BIG):
     if endiannes == ENDIANNESS.BIG:
         return ((value >> n & 1) != 0)
     elif endiannes == ENDIANNESS.LITTLE:
@@ -16,26 +26,66 @@ def get_bit(value, n, endiannes = ENDIANNESS.BIG):
         raise ValueError("Endianness is not properly set - not able to decite which bit to return")
 
 
+def get_bit(value, n, endiannes = ENDIANNESS.BIG):
+    if value == None:
+        raise ValueError(f"Value can not be None")
+
+    size = get_effective_length_in_bytes(value)
+
+    if n == None or n>(size*8-1) or n<0:
+        raise ValueError(f"Wrong bit number passed: {n} which is out of curen variable's size range: 0-{size*8}")
+
+    if endiannes == ENDIANNESS.BIG:
+        return (value >> n & 1)
+    elif endiannes == ENDIANNESS.LITTLE:
+        return (value >> (size*8-1)-n & 1)
+    else:
+        raise ValueError("Endianness is not properly set - not able to decite which bit to return")
+
+
 def set_bit(value, n, endiannes = ENDIANNESS.BIG):
+    if value == None:
+        raise ValueError(f"Value can not be None")
+
+    size = get_effective_length_in_bytes(value)
+    if n == None or n<0:
+        raise ValueError(f"Wrong bit number passed: {n}")
+
     if endiannes == ENDIANNESS.BIG:
         return value | (1 << n)
     elif endiannes == ENDIANNESS.LITTLE:
-        return value | (1 >> n)
+        return value | (1 << (size*8-1)-n)
     else:
-        raise ValueError("Endianness is not properly set - not able to decite which bit to return")
+        raise ValueError("Endianness is not properly set - not able to decide which bit to return")
 
 
 def clear_bit(value, n, endiannes = ENDIANNESS.BIG):
+    if value == None:
+        raise ValueError(f"Value can not be None")
+
+    size = get_effective_length_in_bytes(value)
+
+    if n == None or n>(size*8-1) or n<0:
+        raise ValueError(f"Wrong bit number passed: {n}")
+
     if endiannes == ENDIANNESS.BIG:
         return value & ~(1 << n)
     elif endiannes == ENDIANNESS.LITTLE:
-        return value & ~(1 >> n)
+        return value & ~(1 << (size*8-1)-n)
     else:
-        raise ValueError("Endianness is not properly set - not able to decite which bit to return")
+        raise ValueError("Endianness is not properly set - not able to decide which bit to return")
 
 
 def get_bit_range_as_int(value, start, end, endiannes = ENDIANNESS.BIG):
-    # print(f"Byte to be analysed: {value:02X} = {value:b}")
+    if value == None:
+        raise ValueError(f"Value can not be None")
+
+    size = get_effective_length_in_bytes(value)
+
+    if start == None or start>(size*8-1) or start < 0:
+        raise ValueError(f"Wrong bit number passed: {start}")
+    if end == None or end>(size*8) or end < 1:
+        raise ValueError(f"Wrong bit number passed: {end}")
 
     if start >= end:
         AssertionError(f"'end' sall be bigger than 'start', condition {start} >= {end} is not satisfied")
@@ -47,7 +97,7 @@ def get_bit_range_as_int(value, start, end, endiannes = ENDIANNESS.BIG):
         for num in range(start, end):
             mask = mask | (1 << num)
     else:
-        for num in range(8-end, 8-start):
+        for num in range((size*8)-end, (size*8)-start):
             mask = mask | (1 << num)
 
     if endiannes == ENDIANNESS.BIG:
@@ -56,5 +106,3 @@ def get_bit_range_as_int(value, start, end, endiannes = ENDIANNESS.BIG):
         return (int.from_bytes(value_byte, byteorder = "little") & mask) >> 8-start
 
 
-if __name__ == "__main__":
-    print(get_bit_range_as_int(0x6F, 5, 6))
--- test/BinaryOperations_test.py
+++ test/BinaryOperations_test.py
@@ -6,18 +6,95 @@ import sys
 if not '../bitops' in sys.path:
     sys.path.insert(1, '../bitops')
 
-# from BitParser.SubscriberManager import SubscriberManager
+
+from bitops.BinaryOperations import get_bit, set_bit, clear_bit, get_bit_range_as_int, ENDIANNESS
+
+class TestBinaryOperations:
+    def test_get_bit_ok(self):
+        assert 1 == get_bit(0x0F, 2)
+        assert 0 == get_bit(0x0F, 7)
+        assert 1 != get_bit(0x0F, 2, endiannes=ENDIANNESS.LITTLE)
+        assert 0 != get_bit(0x0F, 7, endiannes=ENDIANNESS.LITTLE)
+
+        assert 1 == get_bit(0x01, 0)
+        assert 1 == get_bit(0x02, 1)
+        assert 1 == get_bit(0x04, 2)
+        assert 1 == get_bit(0x08, 3)
+        assert 1 == get_bit(0x10, 4)
+        assert 1 == get_bit(0x20, 5)
+        assert 1 == get_bit(0x40, 6)
+        assert 1 == get_bit(0x80, 7)
+
+        assert 1 == get_bit(0x01, 7, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x02, 6, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x04, 5, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x08, 4, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x10, 3, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x20, 2, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x40, 1, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x80, 0, endiannes=ENDIANNESS.LITTLE)
+
+    def test_get_bit_wrong_input(self):
+        with pytest.raises(ValueError):
+            assert 1 == get_bit(0xFF, 8)
+            
+        with pytest.raises(ValueError):
+            assert 1 == get_bit(None, 4)
+
+        with pytest.raises(ValueError):
+            assert 1 == get_bit(0x12, None)
+
+    def test_get_bit_long_input(self):
+        assert 1 == get_bit(0x11223344556677889900000000FF01, 0)
+        assert 0 == get_bit(0x11223344556677889900000000FF00, 0)
+        assert 1 == get_bit(0x11223344556677889900000000FF11, 3, endiannes=ENDIANNESS.LITTLE)
+        assert 0 == get_bit(0x01223344556677889900000000FF11, 3, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x01223344556677889900000000FF11, 19, endiannes=ENDIANNESS.LITTLE)
+        assert 0 == get_bit(0x01222344556677889900000000FF11, 19, endiannes=ENDIANNESS.LITTLE)
+        assert 1 == get_bit(0x01223344556677889901000000FF11, 40)
+        assert 0 == get_bit(0x01222344556677889900000000FF11, 40)
 
 
-class TestExample:
-    # def setUpClass():
-    #     print("Wow called")
-    #
-    # def tearDownClass():
-    #     print("Wow teardown called...")
+    def test_set_bit_ok(self):
+        assert 0x04 == set_bit(0x00, 2)
+        assert 0xFF == set_bit(0x7F, 7)
+        assert 0x0800007F == set_bit(0x7F, 27)
+        assert 0x20 == set_bit(0x00, 2, endiannes=ENDIANNESS.LITTLE)
+        assert 0xFF == set_bit(0xFE, 7, endiannes=ENDIANNESS.LITTLE)
+        assert 0x10000001 == set_bit(0x10000000, 31, endiannes=ENDIANNESS.LITTLE)
+        assert 0xFF == set_bit(0x7F, 7)
+        assert 0x20 == set_bit(0x00, 2, endiannes=ENDIANNESS.LITTLE)
+        assert 0xFF == set_bit(0xFE, 7, endiannes=ENDIANNESS.LITTLE)
 
-    def test_ok(self):
-        assert 0 == 0
 
-    def test_ok2(self):
-        assert 0 == 0
+    def test_set_bit_wrong_input(self):
+        with pytest.raises(ValueError):
+            set_bit(None, 4)
+
+        with pytest.raises(ValueError):
+            set_bit(0x12, None)
+
+    def test_clear_bit_ok(self):
+        assert 0x00 == clear_bit(0x04, 2)
+        assert 0x7F == clear_bit(0xFF, 7)
+        assert 0x00 == clear_bit(0x20, 2, endiannes=ENDIANNESS.LITTLE)
+        assert 0xFE == clear_bit(0xFF, 7, endiannes=ENDIANNESS.LITTLE)
+
+
+    def test_clear_bit_wrong_input(self):
+        with pytest.raises(ValueError):
+            clear_bit(0xFF, 8)
+
+        with pytest.raises(ValueError):
+            clear_bit(None, 4)
+
+        with pytest.raises(ValueError):
+            clear_bit(0x12, None)
+
+
+    def test_get_bit_range_as_int(self):
+        assert 7 == get_bit_range_as_int(0x07, 0, 3)
+        assert 1 == get_bit_range_as_int(0x80, 7, 8)
+        assert 8 == get_bit_range_as_int(0x80, 4, 8)
+        assert 2 == get_bit_range_as_int(0x80, 6, 8)
+        assert 3 == get_bit_range_as_int(0xFF, 6, 8)
